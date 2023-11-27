cfebce398e1c39aceaf11ed46553bb4b366abdac
石大大 <128768915+AndyInAi@users.noreply.github.com>
2023-11-27 13:48:22 +0800

    Add files via upload

---
+++ app/controller/Index.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace app\controller;
+
+use app\BaseController;
+
+class Index extends BaseController
+{
+    public function index()
+    {
+	echo "Winter: Hello ThinkPHP";
+        # return '<style>*{ padding: 0; margin: 0; }</style><iframe src="https://www.thinkphp.cn/welcome?version=' . \think\facade\App::version() . '" width="100%" height="100%" frameborder="0" scrolling="auto"></iframe>';
+    }
+
+    public function hello($name = 'ThinkPHP8')
+    {
+        return 'hello,' . $name;
+    }
+}
